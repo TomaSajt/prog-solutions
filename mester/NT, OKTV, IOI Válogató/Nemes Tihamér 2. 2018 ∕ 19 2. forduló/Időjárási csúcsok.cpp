50dde8a9401271afdd3d2bb158a25d8ee51285bd
Toma <tomasajtofficial@gmail.com>
2022-01-05 11:53:59 +0100

    woah

---
+++ mester/NT, OKTV, IOI Válogató/Nemes Tihamér 2. 2018 ∕ 19 2. forduló/Időjárási csúcsok.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define speed ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+typedef long long ll;
+
+int main() {
+    speed;
+    int n, k, l;
+    cin >> n >> k >> l;
+    vector<int> a(n);
+    for (int& i : a) cin >> i;
+    
+
+}
