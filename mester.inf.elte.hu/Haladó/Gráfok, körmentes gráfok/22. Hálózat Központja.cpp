f2a155aec58e86c5f5fbb90927af8f518a06bec6
Toma <tomasajtofficial@gmail.com>
2022-03-30 15:32:50 +0200

    Optimize Hálózati Központ

--- mester.inf.elte.hu/Haladó/Gráfok, körmentes gráfok/22. Hálózat Központja.cpp
+++ mester.inf.elte.hu/Haladó/Gráfok, körmentes gráfok/22. Hálózat Központja.cpp
@@ -18,13 +18,8 @@ int main() {
         g[v].pb(u);
     }
     queue<int> q;
-    for (int i = 1;i <= n;i++) {
-        if (g[i].size() <= 1) {
-            q.push(i);
-            dist[i] = 0;
-            break;
-        }
-    }
+    q.push(1);
+    dist[1] = 0;
     while (!q.empty()) {
         int curr = q.front(); q.pop();
         for (int nei : g[curr]) {
@@ -33,6 +28,7 @@ int main() {
             q.push(nei);
         }
     }
+    //This is one of the farthest points from the root
     int res = max_element(dist.begin(), dist.end()) - dist.begin();
     q.push(res);
     dist.assign(n + 1, -1);
@@ -47,9 +43,12 @@ int main() {
             parent[nei] = curr;
         }
     }
+    //Find the farthest point from the pervious farthes point
+    //The path from those two points will have the root exactly in the middle
     int other = max_element(dist.begin(), dist.end()) - dist.begin();
     int curr = other;
     for (int i = 0;i < dist[other] / 2;i++) curr = parent[curr];
+    //If there are two middle nodes, choose the lower index
     if (dist[other] % 2 != 0 && curr > parent[curr]) curr = parent[curr];
     cout << curr << endl;
 }
