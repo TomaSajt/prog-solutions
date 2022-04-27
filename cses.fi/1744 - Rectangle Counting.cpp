64e196d7db0b9da7d61be3e48202d1fdba2ca161
Toma <tomasajtofficial@gmail.com>
2022-04-27 21:19:00 +0200

    Modify rectangle counting

--- cses.fi/1744 - Rectangle Counting.cpp
+++ cses.fi/1744 - Rectangle Counting.cpp
@@ -1,29 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int DP[501][501];
 
 int cuts(int a, int b) {
-    if (a == b || DP[a][b])
-        return DP[a][b];
+    if (a == b) return 0;
+    if (a > b) swap(a, b);
+    if (DP[a][b]) return DP[a][b];
 
     DP[a][b] = INT_MAX;
+
     for (int i = 1;i <= a / 2;i++) {
-        int res = cuts(i, b) + cuts(a - i, b);
-        DP[a][b] = min(res, DP[a][b]);
+        DP[a][b] = min(DP[a][b], cuts(i, b) + cuts(a - i, b));
+    }
+    for (int j = 1;j <= b / 2;j++) {
+        DP[a][b] = min(DP[a][b], cuts(a, j) + cuts(a, b - j));
     }
 
-    for (int j = 1;j <= b / 2;j++) {
-        int res = cuts(a, j) + cuts(a, b - j);
-        DP[a][b] = min(res, DP[a][b]);
-    }
     return ++DP[a][b];
 }
 
 int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n, m;
     cin >> n >> m;
     cout << cuts(n, m) << endl;
 }
-
-
