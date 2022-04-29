17416784992632a2362b804fa854d8ed6eabfaa3
Toma <tomasajtofficial@gmail.com>
2022-04-29 17:51:17 +0200

    optimize dijkstra a bit

--- codeforces.com/problemset/20C - Dijkstra.cpp
+++ codeforces.com/problemset/20C - Dijkstra.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #define pb push_back
-#define speed ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define speed ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#pragma GCC optimize ("Ofast")
 using namespace std;
 typedef long long ll;
 
@@ -15,41 +16,36 @@ int main() {
         g[u].pb({ w, v });
         g[v].pb({ w, u });
     }
-    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
+    priority_queue<pair<ll, ll>> pq;
     vector<ll> dist(n + 1, LLONG_MAX);
     vector<ll> parent(n + 1, -1);
     pq.push({ 0, 1 });
     dist[1] = 0;
     while (!pq.empty()) {
-        ll u = pq.top().second;
+        auto [a, u] = pq.top();
         pq.pop();
         if (u == n) break;
-        for (auto& e : g[u]) {
-            ll v = e.second;
-            ll w = e.first;
+        if (a != -dist[u]) continue;
+        for (auto& [w, v] : g[u]) {
             if (dist[v] > dist[u] + w) {
                 dist[v] = dist[u] + w;
                 parent[v] = u;
-                pq.push({ dist[v], v });
+                pq.push({ -dist[v], v });
             }
         }
     }
-
     if (dist[n] == LLONG_MAX) {
         cout << -1 << endl;
         return 0;
     }
-    vector<ll> path;
+    stack<ll> path;
     while (n != -1) {
-        path.pb(n);
+        path.push(n);
         n = parent[n];
     }
-    for (ll i = path.size() - 1; i >= 0; i--) {
-        cout << path[i] << " ";
+    while (!path.empty()) {
+        cout << path.top() << ' '; path.pop();
     }
     return 0;
-
-
-
 
 }
