19fbd2c0a8e979180891969a404888a34b660b0b
Toma <tomasajtofficial@gmail.com>
2022-05-01 08:05:59 +0200

    solve nice beatmap :)

---
+++ codeforces.com/problemset/1477C - Nezzar and Nice Beatmap.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define speed ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#pragma GCC optimize ("Ofast")
+using namespace std;
+#define int long long
+
+struct point {
+    int x, y;
+};
+
+inline int dist(point a, point b) {
+    int dx = a.x - b.x, dy = a.y - b.y;
+    return dx * dx + dy * dy;
+}
+#undef int
+int main() {
+#define int long long
+    speed;
+    int n;
+    cin >> n;
+    vector<point> points(n);
+    for (auto& p : points) cin >> p.x >> p.y;
+    vector<bool> used(n);
+    int prev = 0;
+    for (int i = 0;i < n;i++) {
+        int res = -1;
+        int maxd = -1;
+        for (int curr = 0; curr < n; curr++) {
+            int d = dist(points[prev], points[curr]);
+            if (!used[curr] && d > maxd) {
+                maxd = d;
+                res = curr;
+            }
+        }
+        cout << res + 1 << ' ';
+        used[res] = true;
+        prev = res;
+    }
+    cout << endl;
+}
