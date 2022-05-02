8769364c9038c096ca863f7ac4645f4c11ddc845
Toma <tomasajtofficial@gmail.com>
2022-05-02 19:49:18 +0200

    Check ggc exit code to not call a.exe on fail

--- gccr.ps1
+++ gccr.ps1
@@ -1,12 +1,16 @@
 param ($path)
+$ErrorActionPreference = "Stop"
 if($path -eq $null) {
     throw 'Please supply a path to a .cpp file'
 }
 if (-not (Test-Path -Path $path)) {
     throw 'The file does not exist'
 }
-if(-not ($path -match '.cpp')) {
+if(-not $path -match '.cpp') {
     throw 'The supplied file is not a .cpp file'
 }
 g++ $path
+if(-not $LastExitCode -eq 0) {
+    Exit
+}
 ./a.exe
--- gccrf.ps1
+++ gccrf.ps1
@@ -1,14 +1,18 @@
-param ($path, $i='.\input.txt', $o='.\output.txt',$e='.\error.txt')
+param ($path)
+$ErrorActionPreference = "Stop"
 if($path -eq $null) {
     throw 'Please supply a path to a .cpp file'
 }
 if (-not (Test-Path -Path $path)) {
     throw 'The file does not exist'
 }
-if(-not ($path -match '.cpp')) {
+if(-not $path -match '.cpp') {
     throw 'The supplied file is not a .cpp file'
 }
 g++ $path
+if(-not $LastExitCode -eq 0) {
+    Exit
+}
 $processOptions = @{
     FilePath = ".\a.exe"
     RedirectStandardInput = ".\input.txt"
