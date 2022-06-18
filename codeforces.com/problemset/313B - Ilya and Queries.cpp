4de294d5fb71d94965e1f2f046616d66aeb601f3
Toma <tomasajtofficial@gmail.com>
2022-06-18 23:58:41 +0200

    fix silly mistake

--- codeforces.com/problemset/313B - Ilya and Queries.cpp
+++ codeforces.com/problemset/313B - Ilya and Queries.cpp
@@ -6,6 +6,6 @@ int n,l,r,i=1;
 cin>>s>>n;
 n=s.size();
 vector<int>a(n+1);
-for(;i<n;)a[i+1]=a[i]+(s[i]==s[i++-1]);
+for(;i<n;i++)a[i+1]=a[i]+(s[i]==s[i-1]);
 while(cin>>l>>r)cout<<a[r]-a[l]<<'\n';
 }
