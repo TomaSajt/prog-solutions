e9ef3bd84f5e013f22ca4ac1fa544806481fcaf4
Toma <tomasajtofficial@gmail.com>
2022-06-18 23:57:03 +0200

    solve ilya and queries

---
+++ codeforces.com/problemset/313B - Ilya and Queries.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+string s;
+int n,l,r,i=1;
+cin>>s>>n;
+n=s.size();
+vector<int>a(n+1);
+for(;i<n;)a[i+1]=a[i]+(s[i]==s[i++-1]);
+while(cin>>l>>r)cout<<a[r]-a[l]<<'\n';
+}
