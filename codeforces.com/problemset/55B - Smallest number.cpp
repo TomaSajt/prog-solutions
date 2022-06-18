abcb27c0d519b639a9dc7ebfcdbe1fe005781d55
Toma <tomasajtofficial@gmail.com>
2022-06-19 00:48:38 +0200

    New solution to smallest number

--- codeforces.com/problemset/55B - Smallest number.cpp
+++ codeforces.com/problemset/55B - Smallest number.cpp
@@ -1,35 +1,20 @@
 #include <bits/stdc++.h>
-#define pb push_back
 #define speed ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 #pragma GCC optimize ("Ofast")
 using namespace std;
 typedef long long ll;
-
-ll f(char c, ll a, ll b) {
+inline ll f(char c, ll a, ll b) {
     return c == '+' ? a + b : a * b;
 }
-
 int main() {
     speed;
-    vector<ll> v(4);
     vector<char> ops(3);
-    for (ll& i : v)cin >> i;
-    for (char& c : ops)cin >> c;
+    vector<ll> o(4);
+    for (ll& i : o) cin >> i;
+    for (char& c : ops) cin >> c;
     ll best = LLONG_MAX;
-    for (int i = 0;i < 4;i++) {
-        for (int j = i + 1; j < 4;j++) {
-            for (int k = 0;k < 3;k++) {
-                for (int l = k + 1; l < 3;l++) {
-                    auto vc = v;
-                    vc[j] = f(ops[0], vc[i], vc[j]);
-                    vc.erase(vc.begin() + i);
-                    vc[l] = f(ops[1], vc[k], vc[l]);
-                    vc.erase(vc.begin() + k);
-                    ll res = f(ops[2], vc[0], vc[1]);
-                    best = min(best, res);
-                }
-            }
-        }
-    }
-    cout << best;
+    sort(o.begin(), o.end());
+    do best = min({ best, f(ops[2],f(ops[0],o[0],o[1]),f(ops[1],o[2],o[3])), f(ops[2],f(ops[1],f(ops[0],o[0],o[1]),o[2]),o[3]) });
+    while (next_permutation(o.begin(), o.end()));
+    cout << best << endl;
 }
