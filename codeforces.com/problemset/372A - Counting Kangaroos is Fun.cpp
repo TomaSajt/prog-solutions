180bf5b69efbf05092731e2f2767abc3607b9aa7
Toma <tomasajtofficial@gmail.com>
2022-06-19 01:34:55 +0200

    clean up kangaroo counting

--- codeforces.com/problemset/372A - Counting Kangaroos is Fun.cpp
+++ codeforces.com/problemset/372A - Counting Kangaroos is Fun.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
+#define speed ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#pragma GCC optimize ("Ofast")
 using namespace std;
 int main() {
+    speed;
     int n;
     cin >> n;
-    vector<int> v(n);
-    for (int& a : v) cin >> a;
-    sort(v.begin(), v.end());
-    int i, j, lathato = v.size();
-    for (i = 0, j = v.size() / 2; i < v.size() / 2 && j < v.size(); i++) {
-        for (; v[i] * 2 > v[j] && j < v.size(); j++);
-        if (j < v.size()) {
-            v[j] = 0;
-            lathato--;
-        }
+    vector<int> s(n);
+    for (int& a : s) cin >> a;
+    sort(s.begin(), s.end());
+    int i, j, vis = s.size();
+    for (i = 0, j = s.size() / 2; i < s.size() / 2 && j < s.size(); i++) {
+        for (; s[i] * 2 > s[j] && j < s.size(); j++);
+        if (j == s.size()) continue;
+        s[j] = 0;
+        vis--;
     }
-    cout << lathato << endl;
+    cout << vis << endl;
 }
