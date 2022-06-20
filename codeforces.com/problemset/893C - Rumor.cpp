cff5000ebabc543dcc255ef805c305b93c667b30
Toma <tomasajtofficial@gmail.com>
2022-06-20 10:24:25 +0200

    refactor rumor

--- codeforces.com/problemset/893C - Rumor.cpp
+++ codeforces.com/problemset/893C - Rumor.cpp
@@ -4,17 +4,16 @@
 using namespace std;
 typedef long long ll;
 
-int n, m;
+int n, m, best;
 vector<int> c;
 vector<vector<int>> g;
-vector<int> vis;
+vector<bool> vis;
 
-void dfs(int u, int& best) {
-    vis[u] = true;
+void dfs(int u) {
+    vis[u] = 1;
     if (c[u] < best) best = c[u];
-
-    for (auto nei : g[u]) {
-        if (!vis[nei]) dfs(nei, best);
+    for (int nei : g[u]) {
+        if (!vis[nei]) dfs(nei);
     }
 }
 
@@ -27,16 +26,15 @@ int main() {
     for (int i = 1; i <= n; i++) cin >> c[i];
     while (m--) {
         int u, v; cin >> u >> v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        g[u].pb(v);
+        g[v].pb(u);
     }
     ll sum = 0;
     for (int i = 1; i <= n;i++) {
         if (vis[i]) continue;
-        int best = INT_MAX;
-        dfs(i, best);
+        best = INT_MAX;
+        dfs(i);
         sum += best;
     }
     cout << sum;
-
 }
