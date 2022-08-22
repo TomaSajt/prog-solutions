6c9ebf057aba89d54e613a51b044b372d8f24e92
Toma <tomasajtofficial@gmail.com>
2022-08-22 15:26:17 +0200

    pogs

--- rosalind/LCSM.cpp
+++ rosalind/LCSM.cpp
@@ -1,5 +1,82 @@
 #include "_helpers.hpp"
 
+inline bool leq(int a1, int a2, int b1, int b2) { // lexic. order for pairs
+    return(a1 < b1 || a1 == b1 && a2 <= b2);
+}                                                   // and triples
+inline bool leq(int a1, int a2, int a3, int b1, int b2, int b3) {
+    return(a1 < b1 || a1 == b1 && leq(a2, a3, b2, b3));
+}
+// stably sort a[0..n-1] to b[0..n-1] with keys in 0..K from r
+static void radixPass(const vector<int>& a, vector<int>& b, const vector<int>& r, int r_offset, int n, int K) { // count occurrences
+    vector<int> c(K + 1, 0);
+    for (int i = 0; i < n; i++) c[r[a[i] + r_offset]]++;    // count occurences
+    for (int i = 0, sum = 0; i <= K; i++) { // exclusive prefix sums
+        int t = c[i];  c[i] = sum;  sum += t;
+    }
+    for (int i = 0; i < n; i++) b[c[r[a[i] + r_offset]]++] = a[i];      // sort
+}
+
+// find the suffix array SA of s[0..n-1] in {1..K}^n
+// require s[n]=s[n+1]=s[n+2]=0, n>=2
+void suffixArray(vector<int>& s, vector<int>& SA, int n, int K) {
+    int n0 = (n + 2) / 3, n1 = (n + 1) / 3, n2 = n / 3, n02 = n0 + n2;
+    vector<int> s12(n02 + 3, 0), SA12(n02 + 3, 0), s0(n0), SA0(n0);
+
+    // generate positions of mod 1 and mod  2 suffixes
+    // the "+(n0-n1)" adds a dummy mod 1 suffix if n%3 == 1
+    for (int i = 0, j = 0; i < n + (n0 - n1); i++) if (i % 3 != 0) s12[j++] = i;
+
+    // lsb radix sort the mod 1 and mod 2 triples
+    radixPass(s12, SA12, s, 2, n02, K);
+    radixPass(SA12, s12, s, 1, n02, K);
+    radixPass(s12, SA12, s, 0, n02, K);
+
+    // find lexicographic names of triples
+    int name = 0, c0 = -1, c1 = -1, c2 = -1;
+    for (int i = 0; i < n02; i++) {
+        if (s[SA12[i]] != c0 || s[SA12[i] + 1] != c1 || s[SA12[i] + 2] != c2) {
+            name++;  c0 = s[SA12[i]];  c1 = s[SA12[i] + 1];  c2 = s[SA12[i] + 2];
+        }
+        if (SA12[i] % 3 == 1) { s12[SA12[i] / 3] = name; } // left half
+        else { s12[SA12[i] / 3 + n0] = name; } // right half
+    }
+
+    // recurse if names are not yet unique
+    if (name < n02) {
+        suffixArray(s12, SA12, n02, name);
+        // store unique names in s12 using the suffix array 
+        for (int i = 0; i < n02; i++) s12[SA12[i]] = i + 1;
+    }
+    else // generate the suffix array of s12 directly
+        for (int i = 0; i < n02; i++) SA12[s12[i] - 1] = i;
+
+    // stably sort the mod 0 suffixes from SA12 by their first character
+    for (int i = 0, j = 0; i < n02; i++) if (SA12[i] < n0) s0[j++] = 3 * SA12[i];
+    radixPass(s0, SA0, s, 0, n0, K);
+
+    // merge sorted SA0 suffixes and sorted SA12 suffixes
+    for (int p = 0, t = n0 - n1, k = 0; k < n; k++) {
+#define GetI() (SA12[t] < n0 ? SA12[t] * 3 + 1 : (SA12[t] - n0) * 3 + 2)
+        int i = GetI(); // pos of current offset 12 suffix
+        int j = SA0[p]; // pos of current offset 0  suffix
+        if (SA12[t] < n0 ?
+            leq(s[i], s12[SA12[t] + n0], s[j], s12[j / 3]) :
+            leq(s[i], s[i + 1], s12[SA12[t] - n0 + 1], s[j], s[j + 1], s12[j / 3 + n0])) { // suffix from SA12 is smaller
+            SA[k] = i;  t++;
+            if (t == n02) { // done --- only SA0 suffixes left
+                for (k++; p < n0; p++, k++) SA[k] = SA0[p];
+            }
+        }
+        else {
+            SA[k] = j;  p++;
+            if (p == n0) { // done --- only SA12 suffixes left
+                for (k++; t < n02; t++, k++) SA[k] = GetI();
+            }
+        }
+    }
+#undef GetI
+}
+
 class compact_min_segment_tree {
     int n;
     int UNIQUE = 93136074;
@@ -27,17 +104,13 @@ public:
     int query(int l, int r) {
         int res = UNIQUE;
         for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-            if ((l & 1) != 0) res = function(res, tree[l++]);
-            if ((r & 1) != 0) res = function(res, tree[--r]);
+            if (l & 1) res = function(res, tree[l++]);
+            if (r & 1) res = function(res, tree[--r]);
         }
         if (res == UNIQUE) throw runtime_error("UNIQUE should not be the return value.");
         return res;
     }
 };
-
-
-
-
 
 struct suffix_rank_tuple {
     int firstHalf, secondHalf, originalIndex;
@@ -48,8 +121,7 @@ struct suffix_rank_tuple {
     }
 };
 
-class suffix_array {
-public:
+struct suffix_array {
     vector<int> sa;
     vector<int> lcp;
     vector<int> T;
@@ -94,7 +166,6 @@ public:
         for (int i = 0; i < N; i++) sa[i] = ranks[i].originalIndex;
     }
 
-
     void construct_lcp() {
         lcp.assign(N, 0);
         vector<int> inv(N);
@@ -117,79 +188,93 @@ string retrieveString(int i, int len, const vector<int>& text, int shift) {
     return str;
 }
 
-ifstream in("C:/Users/Toma/Downloads/rosalind_lcsm(2).txt");
+ifstream in("C:/Users/Toma/Downloads/rosalind_lcsm.txt");
 int main() {
     vector<string> strings;
     auto fasta = rosalind::getFasta(in);
     for (auto& [name, str] : fasta) {
         strings.push_back(str);
     }
-
+    strings = { "willy", "winners" };
     int num_sentinels = strings.size();
-    int total_length = num_sentinels;
-    for (auto& str : strings) total_length += str.size();
+    int n = num_sentinels;
+    for (auto& str : strings) n += str.size();
 
 
-    vector<int> imap(total_length);
+    vector<int> imap(n);
     int lowest_ascii_value = INT_MAX, highest_ascii_value = INT_MIN;
     for (int i = 0, k = 0; i < strings.size(); i++) {
-        for (int j = 0; j < strings[i].length(); j++) {
+        for (int j = 0; j < strings[i].size(); j++) {
             int asciiVal = strings[i][j];
             lowest_ascii_value = min(lowest_ascii_value, (int)strings[i][j]);
             highest_ascii_value = max(highest_ascii_value, (int)strings[i][j]);
             imap[k++] = i;
         }
-        // ith sentinel belongs to string i
-        imap[k++] = i;
+        imap[k++] = i; // ith sentinel belongs to string i
     }
 
 
     int shift = num_sentinels - lowest_ascii_value;
 
 
-    vector<int> text(total_length);
+    vector<int> text(n);
+    vector<int> s(n + 3), SA(n + 3); // There needs to be 3 extra zeroes at the end
     int sentinel = 0;
     for (int i = 0, k = 0; i < strings.size(); i++) {
         for (int j = 0; j < strings[i].size(); j++) {
-            text[k++] = strings[i][j] + shift;
+            text[k] = strings[i][j] + shift;
+            s[k] = text[k] + 1;
+            k++;
         }
-        text[k++] = sentinel++;
+        text[k] = sentinel;
+        s[k] = text[k] + 1;
+        sentinel++;
+        k++;
     }
+    suffixArray(s, SA, n, 1 + highest_ascii_value - lowest_ascii_value + num_sentinels);
+    SA.pop_back();
+    SA.pop_back();
+    SA.pop_back();
+
+
     suffix_array bruh(text);
     bruh.construct_sa();
     bruh.construct_lcp();
     auto sa = bruh.sa;
+    cout << (SA == sa) << endl;
+
+    //cout << sa;
     auto lcp = bruh.lcp;
     int k = strings.size();
     compact_min_segment_tree tree(bruh.lcp);
 
     set<string> lcss;
     int best_lcs_len = 0;
-    int lo = num_sentinels;
-    int hi = num_sentinels;
+    int lo = num_sentinels; // inclusive
+    int hi = num_sentinels; // exclusive
 
     map<int, int> col_count;
 
-    col_count[imap[sa[hi]]]++;
 
+    // window is on the interval [lo, hi)
     while (true) {
-        bool shrinkWindow = (hi == total_length - 1) || col_count.size() >= k;
+        bool shrinkWindow = hi == n || col_count.size() >= k;
         if (shrinkWindow) {
             int col = imap[sa[lo]];
             if (--col_count[col] == 0) col_count.erase(col);
             lo++;
         }
         else {
-            hi++;
             int col = imap[sa[hi]];
             col_count[col]++;
+            hi++;
         }
+        cout << lo << ' ' << hi << '\n';
+        if (lo == n - 1) break;
 
-        if (lo == total_length - 1) break;
-
-        if (lo == hi) continue;
+        if (hi - lo <= 1) continue;
         // min query on the interval [lo + 1, hi] (lo + 1 because first lcp value in window is not used)
-        int min_window_lcp = tree.query(lo + 1, hi + 1);
+        int min_window_lcp = tree.query(lo + 1, hi);
 
         if (col_count.size() < k) continue;
 
