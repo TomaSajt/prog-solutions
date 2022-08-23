a5b02c1db28fdf94a8857aa2715822f3493294c8
Toma <tomasajtofficial@gmail.com>
2022-08-23 17:13:36 +0200

    mst refactor

--- spoj/MST.cpp
+++ spoj/MST.cpp
@@ -1,32 +1,22 @@
 #include <bits/stdc++.h>
-#define pb push_back
 #define speed ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 #pragma GCC optimize ("Ofast")
 using namespace std;
-typedef long long ll;
-vector<int> parents;
-vector<int> sizes;
-
+vector<int> parents, sizes;
 struct edge {
     int s, e, w;
-    bool operator<(const edge& other) const {
-        return w < other.w;
-    }
 };
-
 int find_root(int a) {
     if (parents[a] == a) return a;
     return parents[a] = find_root(parents[a]);
 }
-
 void merge(int a, int b) {
     a = find_root(a);
     b = find_root(b);
-    if (sizes[a] > sizes[b]) swap(a, b); // a kisebb lesz, mint b
+    if (sizes[a] > sizes[b]) swap(a, b); // a will be shorter than b
     parents[a] = b;
     sizes[b] += sizes[a];
 }
-
 int main() {
     speed;
     int n, m;
@@ -36,7 +26,7 @@ int main() {
     sizes.resize(n + 1, 1);
     vector<edge> edges(m);
     for (auto& edge : edges) cin >> edge.s >> edge.e >> edge.w;
-    sort(edges.begin(), edges.end());
+    sort(edges.begin(), edges.end(), [](auto& a, auto& b) {return a.w < b.w;}); // növekvő sorrend
     int total = 0;
     for (auto [s, e, w] : edges) {
         if (find_root(s) != find_root(e)) {
