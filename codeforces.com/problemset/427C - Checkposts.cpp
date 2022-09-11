6357e88cc3b18287ae02e9c58622ce440bd09027
Toma <tomasajtofficial@gmail.com>
2022-09-11 11:33:14 +0200

    refactor checkposts

--- codeforces.com/problemset/427C - Checkposts.cpp
+++ codeforces.com/problemset/427C - Checkposts.cpp
@@ -1,62 +1,68 @@
 #include <bits/stdc++.h>
-#define pb push_back
-#define speed ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#pragma GCC optimize ("Ofast")
+#define speed ios::sync_with_stdio(0);cin.tie(0)
 using namespace std;
 typedef long long ll;
 const int MOD = 1e9 + 7;
 
-int n;
-vector<int> prices, order;
-vector<bool> visited;
+vector<int> costs, ord;
+vector<bool> vis;
 vector<vector<int>> g, gt, comps;
 
-
-void dfs1(int u) {
-    visited[u] = 1;
-    for (int nei : g[u]) if (!visited[nei]) dfs1(nei);
-    order.pb(u);
+void dfs(int u) {
+    vis[u] = 1;
+    for (int v : g[u]) {
+        if (!vis[v]) dfs(v);
+    }
+    ord.push_back(u);
 }
 
 void dfs2(int u) {
-    visited[u] = 1;
-    comps.back().pb(u);
-    for (int nei : gt[u]) if (!visited[nei]) dfs2(nei);
+    vis[u] = 1;
+    comps.back().push_back(u);
+    for (int v : gt[u]) {
+        if (!vis[v]) dfs2(v);
+    }
 }
-
 
 int main() {
     speed;
+    int n, m;
     cin >> n;
 
     g.resize(n + 1);
     gt.resize(n + 1);
-    prices.resize(n + 1);
-    visited.resize(n + 1);
+    costs.resize(n + 1);
+    vis.resize(n + 1);
 
-    for (int i = 1;i <= n;i++) cin >> prices[i];
-
-    int m; cin >> m;
+    for (int i = 1; i <= n; i++) cin >> costs[i];
+    cin >> m;
 
     while (m--) {
         int u, v;
         cin >> u >> v;
-        g[u].pb(v);
-        gt[v].pb(u);
+        g[u].push_back(v);
+        gt[v].push_back(u);
     }
 
-    for (int i = 1;i <= n;i++) if (!visited[i]) dfs1(i);
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i]) dfs(i);
+    }
+    
+    reverse(ord.begin(), ord.end());
+    vis.assign(n + 1, 0);
 
-    reverse(order.begin(), order.end());
-    visited.assign(n + 1, 0);
-
-    for (int i : order) if (!visited[i]) comps.pb({}), dfs2(i);
+    for (int i : ord) {
+        if (!vis[i]) {
+            comps.emplace_back();
+            dfs2(i);
+        }
+    }
 
     ll sum = 0;
     ll res = 1;
     for (auto& comp : comps) {
-        ll val = prices[*min_element(comp.begin(), comp.end(), [&](int a, int b) {return prices[a] < prices[b];})];
-        ll c = count_if(comp.begin(), comp.end(), [&](int a) {return prices[a] == val;});
+        ll val = costs[*min_element(comp.begin(), comp.end(), [&](int a, int b) {return costs[a] < costs[b]; })];
+        ll c = count_if(comp.begin(), comp.end(), [&](int a) {return costs[a] == val; });
         sum += val;
         res *= c;
         res %= MOD;
