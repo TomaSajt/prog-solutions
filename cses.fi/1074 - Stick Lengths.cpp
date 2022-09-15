e1fba5a148d6fbba120fe0efa7265dadb622a5ff
Toma <tomasajtofficial@gmail.com>
2022-09-15 23:26:19 +0200

    rework stick lengths

--- cses.fi/1074 - Stick Lengths.cpp
+++ cses.fi/1074 - Stick Lengths.cpp
@@ -4,18 +4,9 @@ using namespace std;
 int main() {
     int n; cin >> n;
     vector<int> v(n);
-    map<int, int> m;
-    for (auto& p : v) {
-        cin >> p;
-        m[p]++;
-    }
-    auto lit = m.begin(), rit = --m.end();
-    while (lit != rit) {
-        int lv = lit->second, rv = rit->second;
-        if (lv < rv) (++lit)->second += lv;
-        else (--rit)->second += rv;
-    }
-    int x = lit->first;
+    for (auto& p : v) cin >> p;
+    sort(v.begin(), v.end());
+    int x = v[(n - 1) / 2];
     long long res = 0;
     for (auto& p : v) res += abs(p - x);
     cout << res;
