df88fbaab3b7666385784700d6d776c0cd30ffe6
Toma <tomasajtofficial@gmail.com>
2022-09-16 00:36:36 +0200

    refactor mail delivery

--- cses.fi/1691 - Mail Delivery.cpp
+++ cses.fi/1691 - Mail Delivery.cpp
@@ -2,7 +2,6 @@
 #define speed ios::sync_with_stdio(0);cin.tie(0)
 using namespace std;
 
-int n, m;
 vector<set<int>> g;
 vector<bool> vis;
 
@@ -14,6 +13,7 @@ void dfs(int u) {
 
 int main() {
     speed;
+    int n, m;
     cin >> n >> m;
     g.resize(n + 1);
     vis.resize(n + 1);
@@ -25,28 +25,24 @@ int main() {
     }
     dfs(1);
     for (int i = 1; i <= n; i++) {
-        if (g[i].size() % 2 == 1 || (!vis[i] && g[i].size() > 0)) {
+        if (g[i].size() % 2 == 1 || (!vis[i] && !g[i].empty())) {
             cout << "IMPOSSIBLE";
             return 0;
         }
     }
     stack<int> s;
     vector<int> path;
-    int cur = 1;
-    while (!s.empty() || g[cur].size() > 0) {
-        if (g[cur].size() == 0) {
-            path.push_back(cur);
-            cur = s.top();
-            s.pop();
-        }
-        else {
+    s.push(1);
+    while (!s.empty()) {
+        int cur = s.top(); s.pop();
+        while (!g[cur].empty()) {
+            int nei = *g[cur].begin();
+            g[cur].erase(nei);
+            g[nei].erase(cur);
             s.push(cur);
-            int v = *g[cur].begin();
-            g[cur].erase(v);
-            g[v].erase(cur);
-            cur = v;
+            cur = nei;
         }
+        path.push_back(cur);
     }
     for (auto u : path) cout << u << ' ';
-    cout << cur << endl;
 }
