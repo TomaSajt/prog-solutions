72302c8139e2e5627ac74e46a2c568c9c2861f3d
Toma <tomasajtofficial@gmail.com>
2022-09-18 00:00:15 +0200

    expand two knights

--- cses.fi/1072 - Two Knights.cpp
+++ cses.fi/1072 - Two Knights.cpp
@@ -1,2 +1,7 @@
-#include<bits/stdc++.h>
-int main(int n){std::cin>>n;for(long long i=0;i<n;)std::cout<<(i*i-i+2)*(i+5)*i++/2<<'\n';}
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for (long long i = 0; i < n; i++) cout << (i * i - i + 2) * (i + 5) * i / 2 << '\n';
+}
