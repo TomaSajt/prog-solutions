7659e561d619af50f90ec05e8a0f663cf2fcd997
Toma <tomasajtofficial@gmail.com>
2022-10-09 11:14:14 +0200

    refactor

--- spoj/SUBMERGE.cpp
+++ spoj/SUBMERGE.cpp
@@ -1,63 +1,51 @@
 #include <bits/stdc++.h>
-#define pb push_back
-#define speed ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#pragma GCC optimize ("Ofast")
+#define speed ios::sync_with_stdio(0);cin.tie(0)
 using namespace std;
-#define int long long
-
-
 vector<vector<int>> g;
 vector<int> d, l;
-set<int> elvagok;
+set<int> res;
 int t = 0;
 int c = 0;
 
-
-
-void dfs(int curr, int p) {
-    d[curr] = l[curr] = ++t;
-    for (auto child : g[curr]) {
-        if (child == p) continue;
-        if (d[child]) {
-            l[curr] = min(l[curr], d[child]);
+void dfs(int u) {
+    d[u] = l[u] = ++t;
+    for (auto v : g[u]) {
+        if (d[v]) {
+            // vissza-él logika
+            // csak a d[v]-t nézzük az l[v] helyett, mert ezzel körbemegyünk felesleges egy körön
+            l[u] = min(l[u], d[v]);
         }
         else {
-            if (curr == 1) c++;
-            //fa-él
-            dfs(child, curr);
-            l[curr] = min(l[curr], l[child]);
-
-            if (curr != 1 && d[curr] <= l[child]) {
-                elvagok.insert(curr);
+            // fa-él logika
+            // a kezdeti csúcsot külön kell kezelni, mert sosem lehet fölémenni
+            // megszámoljuk a belőle kimenő fa-csúcsokat, és ha több mint 2, akkor elvágó
+            if (u == 1) c++;
+            dfs(v);
+            l[u] = min(l[u], l[v]);
+            if (u != 1 && d[u] <= l[v]) { // ha a gyerek minimum csak az u-ig tud visszajutni, akkor az u elvágó csúcs
+                res.insert(u);
             }
         }
     }
-
 }
 
-
-
-
-#undef int
 int main() {
-#define int long long
     speed;
     int n, m;
-    while (cin >> n >> m && n != 0 && m != 0) {
+    while (cin >> n >> m && n) {
         t = 0;
         c = 0;
         g.assign(n + 1, {});
         d.assign(n + 1, 0);
         l.assign(n + 1, 0);
-        elvagok.clear();
+        res.clear();
         while (m--) {
             int u, v;
             cin >> u >> v;
-            g[u].pb(v);
-            g[v].pb(u);
+            g[u].push_back(v);
+            g[v].push_back(u);
         }
-        dfs(1, -1);
-        cout << elvagok.size() + (c > 1) << endl;
+        dfs(1);
+        cout << res.size() + (c >= 2) << endl;
     }
-
 }
