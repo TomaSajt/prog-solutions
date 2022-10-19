fdeee405ccdf5888b4441127713b4f5904a48727
Toma <tomasajtofficial@gmail.com>
2022-10-19 21:27:54 +0200

    update run scripts

--- g++rf.ps1
+++ g++rf.ps1
@@ -1,14 +1,4 @@
 param ($path)
-$ErrorActionPreference = "Stop"
-if($path -eq $null) {
-    throw 'Please supply a path to a .cpp file'
-}
-if (-not (Test-Path -Path $path)) {
-    throw 'The file does not exist'
-}
-if(-not $path -match '.cpp') {
-    throw 'The supplied file is not a .cpp file'
-}
 g++ $path
 if(-not $LastExitCode -eq 0) {
     Exit
