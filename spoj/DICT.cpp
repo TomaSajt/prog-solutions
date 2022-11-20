6be40f84ba552d617d0a4893df7f0287c9d4ce58
Toma <tomasajtofficial@gmail.com>
2022-11-20 11:05:57 +0100

    refactor dict

--- spoj/DICT.cpp
+++ spoj/DICT.cpp
@@ -9,15 +9,13 @@ struct Node {
 
 vector<string> words;
 
-int chtoi(char ch) { return ch - 'a'; }
-
 void insert(Node* node, int word_id, int ind) {
     auto& str = words[word_id];
     if (ind == str.size()) {
         node->word_id = word_id;
         return;
     }
-    auto& next = node->next[chtoi(str[ind])];
+    auto& next = node->next[str[ind] - 'a'];
     if (next == nullptr) next = new Node;
     insert(next, word_id, ind + 1);
 }
@@ -28,8 +26,7 @@ void disp_all(Node* node) {
     for (auto next : node->next) disp_all(next);
 }
 
-
-bool find_disp(Node* node, const string& str, int ind) {
+void find_disp(Node* node, const string& str, int ind) {
     if (node == nullptr) {
         cout << "No match." << '\n';
         return;
@@ -38,13 +35,12 @@ bool find_disp(Node* node, const string& str, int ind) {
         for (auto next : node->next) disp_all(next);
         return;
     }
-    return find_disp(node->next[chtoi(str[ind])], str, ind + 1);
+    return find_disp(node->next[str[ind] - 'a'], str, ind + 1);
 }
 
 int main() {
     speed;
-    int n;
-    cin >> n;
+    int n; cin >> n;
     words.resize(n);
     Node root;
     for (int i = 0; i < n; i++) {
@@ -53,8 +49,7 @@ int main() {
     }
     cin >> n;
     for (int i = 1; i <= n; i++) {
-        string s;
-        cin >> s;
+        string s; cin >> s;
         cout << "Case #" << i << ":\n";
         find_disp(&root, s, 0);
     }
