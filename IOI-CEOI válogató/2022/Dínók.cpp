b63fccbf87bf391d3fde8203834be87da566a85f
Toma <tomasajtofficial@gmail.com>
2022-12-16 22:14:27 +0100

    Actually solve dinok

--- IOI-CEOI válogató/2022/Dínók.cpp
+++ IOI-CEOI válogató/2022/Dínók.cpp
@@ -1,116 +1,36 @@
 #include <bits/stdc++.h>
+#define speed ios::sync_with_stdio(0);cin.tie(0)
 using namespace std;
-#define speed    \
-    cout.tie(0); \
-    cin.tie(0);   \
-    ios::sync_with_stdio(0)
 
-int main()
-{
+int main() {
     speed;
     int n, m;
     cin >> n >> m;
-    while (m--)
-    {
+    vector<vector<int>> g(2 * n);
+    vector<int> deps(2 * n), pos(2 * n);
+    auto makeEdge = [&](int u, int v) { g[u].push_back(v); deps[v]++; };
+    for (int i = 0; i < n; i++) makeEdge(2 * i, 2 * i + 1);
+    while (m--) {
         int t, a, b;
-        cin >> t >> a >> b;
+        cin >> t >> a >> b; --a, --b;
+        if (t == 1) makeEdge(2 * b, 2 * a + 1), makeEdge(2 * a, 2 * b + 1);
+        else makeEdge(2 * a + 1, 2 * b);
     }
-    for (int i = 1; i <= n; i++)
-    {
-        cout << i << ' ' << 2 * n - i + 1 << endl;
+    queue<int> q;
+    for (int i = 0; i < n; i++) {
+        if (deps[2 * i] == 0) q.push(2 * i);
     }
+    int cnt = 0;
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        pos[u] = ++cnt;
+        for (int v : g[u]) {
+            if (--deps[v] == 0) q.push(v);
+        }
+    }
+    if (cnt == 2 * n) {
+        cout << "IGEN";
+        for (int i = 0; i < n; i++) cout << '\n' << pos[2 * i] << ' ' << pos[2 * i + 1];
+    }
+    else cout << "NEM";
 }
-
-/*
-vector<int> befok;
-vector<bool> vis;
-vector<vector<int>> g;
-vector<int> order;
-
-vector<pair<int, int>> inters;
-
-void dfs(int u)
-{
-    order.push_back(u);
-    vis[u] = true;
-    for (int v : g[u])
-    {
-        befok[v]--;
-        if (!vis[v] && befok[v] == 0)
-        {
-            dfs(v);
-        }
-    }
-}
-
-int veg(int a)
-{
-    return 2 * a + 1;
-}
-int kezd(int a)
-{
-    return 2 * a;
-}
-int main()
-{
-    speed;
-    int n, m;
-    cin >> n >> m;
-    g.resize(2 * (n + 1));
-    vis.resize(2 * (n + 1));
-    befok.resize(2 * (n + 1));
-    inters.resize(n + 1);
-    for (int i = 1; i <= n; i++)
-    {
-        g[kezd(i)].push_back(veg(i));
-        befok[veg(i)]++;
-    }
-    while (m--)
-    {
-        int t, a, b;
-        cin >> t >> a >> b;
-        if (a > b)
-            swap(a, b);
-        if (t == 1)
-        {
-            g[kezd(a)].push_back(veg(b));
-            g[kezd(b)].push_back(veg(a));
-            befok[veg(b)]++;
-            befok[veg(a)]++;
-        }
-        else
-        {
-            g[veg(a)].push_back(kezd(b));
-            befok[kezd(b)]++;
-        }
-    }
-    for (int i = 2; i <= 2 * n + 1; i++)
-    {
-        if (!vis[i] && befok[i] == 0)
-            dfs(i);
-    }
-    for (int i = 2; i <= 2 * n + 1; i++)
-    {
-        if (befok[i] != 0)
-        {
-            cout << "NEM" << endl;
-            return 0;
-        }
-    }
-    for (int i = 1; i <= order.size(); i++)
-    {
-        int curr = order[i - 1];
-        if (curr % 2 == 0)
-        {
-            inters[curr / 2].first = i;
-        }
-        else
-        {
-            inters[curr / 2].second = i;
-        }
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        cout << inters[i].first << ' ' << inters[i].second << endl;
-    }
-}*/
