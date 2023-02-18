8bdc850ce13fecdf696fabd8aa1e6fe9b84c7e1a
Toma <tomasajtofficial@gmail.com>
2023-02-18 12:45:13 +0100

    refactor switch the ligths

--- onlinejudge/11974 - Switch The Lights.cpp
+++ onlinejudge/11974 - Switch The Lights.cpp
@@ -1,41 +1,41 @@
 #include <bits/stdc++.h>
-#define speed ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#pragma GCC optimize ("Ofast")
+#define speed ios::sync_with_stdio(0);cin.tie(0)
 using namespace std;
-typedef long long ll;
+
+
+int solve(int n, int m) {
+    vector<int> switches(m);
+    for (int& sw : switches) {
+        for (int i = 0; i < n; i++) {
+            bool b; cin >> b;
+            sw = (sw << 1) | b;
+        }
+    }
+    queue<int> q;
+    vector<int> dist(1 << n, INT_MAX);
+    dist[(1 << n) - 1] = 0;
+    q.push((1 << n) - 1);
+    while (!q.empty()) {
+        int currState = q.front(); q.pop();
+        for (auto& sw : switches) {
+            int nextState = currState ^ sw;
+            if (dist[nextState] != INT_MAX) continue;
+            dist[nextState] = dist[currState] + 1;
+            q.push(nextState);
+        }
+    }
+    return dist[0];
+}
 
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    int t;
-    cin >> t;
-    for (int k = 1; k <= t;k++) {
+    speed;
+    int t; cin >> t;
+    for (int i = 1; i <= t; i++) {
         int n, m;
         cin >> n >> m;
-        vector<int> list(m);
-        for (int& sw : list) {
-            for (int i = 0; i < n;i++) {
-                sw *= 2;
-                bool b;
-                cin >> b;
-                sw += b;
-            }
-        }
-        queue<int> q;
-        vector<int> dist(1 << n, INT_MAX);
-        dist[(1 << n) - 1] = 0;
-        q.push((1 << n) - 1);
-        while (!q.empty()) {
-            int pos = q.front(); q.pop();
-            for (int& sw : list) {
-                int next = pos ^ sw;
-                if (dist[next] == INT_MAX) {
-                    dist[next] = dist[pos] + 1;
-                    q.push(next);
-                }
-            }
-        }
-        cout << "Case " << k << ": ";
-        if (dist[0] == INT_MAX) cout << "IMPOSSIBLE\n";
-        else cout << dist[0] << '\n';
+        cout << "Case " << i << ": ";
+        int res = solve(n, m);
+        if (res == INT_MAX) cout << "IMPOSSIBLE\n";
+        else cout << res << '\n';
     }
 }
