f989cf8ced298e578aebbbd2d16ee2388c921bde
Toma <62384384+TomaSajt@users.noreply.github.com>
2023-04-09 17:54:43 +0200

    Refactor code

--- CSES/1676 - Road Construction.cpp
+++ CSES/1676 - Road Construction.cpp
@@ -1,38 +1,43 @@
 #include <bits/stdc++.h>
-#define speed ios::sync_with_stdio(0);cin.tie(0)
 using namespace std;
-typedef long long ll;
 
-vector<int> parent, depth, cnt;
-int max_cnt = 1;
+vector<int> parent, depth, comp_size;
+int best_size = 1;
 
 int find(int u) {
-    if (parent[u] == u) return u;
-    return parent[u] = find(parent[u]);
+  if (parent[u] == u)
+    return u;
+  return parent[u] = find(parent[u]);
 }
 
-void merge(int u, int v) {
-    u = find(u), v = find(v);
-    if (depth[u] < depth[v]) swap(u, v);
-    parent[v] = u;
-    if (depth[u] == depth[v]) depth[u]++;
-    cnt[u] += cnt[v];
-    max_cnt = max(max_cnt, cnt[u]);
+bool try_merge(int u, int v) {
+  u = find(u), v = find(v);
+  if (u == v)
+    return false;
+  if (depth[u] < depth[v])
+    swap(u, v);
+  parent[v] = u;
+  if (depth[u] == depth[v])
+    depth[u]++;
+  comp_size[u] += comp_size[v];
+  best_size = max(best_size, comp_size[u]);
+  return true;
 }
 
 int main() {
-    speed;
-    int n, m;
-    cin >> n >> m;
-    parent.resize(n + 1);
-    iota(parent.begin(), parent.end(), 0);
-    depth.resize(n + 1, 1);
-    cnt.resize(n + 1, 1);
-    int c = n;
-    while (m--) {
-        int u, v;
-        cin >> u >> v;
-        if (find(u) != find(v)) merge(u, v), c--;
-        cout << c << ' ' << max_cnt << '\n';
-    }
+  cin.tie(0), ios::sync_with_stdio(0);
+  int n, m;
+  cin >> n >> m;
+  parent.resize(n + 1);
+  iota(parent.begin(), parent.end(), 0);
+  depth.resize(n + 1, 1);
+  comp_size.resize(n + 1, 1);
+  int comps = n;
+  while (m--) {
+    int u, v;
+    cin >> u >> v;
+    if (try_merge(u, v))
+      comps--;
+    cout << comps << ' ' << best_size << '\n';
+  }
 }
