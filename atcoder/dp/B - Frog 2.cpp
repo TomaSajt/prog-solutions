9fe83aa04320f2ce644c6a66874babaa73d5a594
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-08-06 19:50:22 +0200

    Separate Frog

---
+++ atcoder/dp/B - Frog 2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+
+  vector<int> v(n + 1);
+  for (int i = 1; i <= n; i++) cin >> v[i];
+
+  vector<int> dp(n + 1, INT_MAX);
+  dp[1] = 0;
+
+  for (int i = 2; i <= n; i++) {
+    for (int j = 1; j <= k; j++) {
+      if (i - j >= 1) {
+        dp[i] = min(dp[i], dp[i - j] + abs(v[i] - v[i - j]));
+      }
+    }
+  }
+
+  cout << dp[n] << endl;
+}
