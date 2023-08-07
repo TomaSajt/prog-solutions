0b6e32054c7ac78f0dfe4299bbb5bdda6dd167dc
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-08-07 13:45:59 +0200

    Refactor Coin Combinations

--- CSES/1635 - Coin Combinations I.cpp
+++ CSES/1635 - Coin Combinations I.cpp
@@ -1,19 +1,23 @@
 #include <bits/stdc++.h>
-#define speed ios::sync_with_stdio(0);cin.tie(0)
 using namespace std;
 const int mod = 1e9 + 7;
 
 int main() {
-    speed;
-    int n, x;
-    cin >> n >> x;
-    vector<int> dp(x + 1), cv(n);
-    for (auto& c : cv) cin >> c;
-    dp[0] = 1;
-    for (int i = 1; i <= x; i++) {
-        for (int c : cv) {
-            if (c <= i) dp[i] = (dp[i] + dp[i - c]) % mod;
-        }
+  ios::sync_with_stdio(0), cin.tie(0);
+  int n, x;
+  cin >> n >> x;
+
+  vector<int> cv(n);
+  for (auto& c : cv) cin >> c;
+
+  vector<int> dp(x + 1);
+  dp[0] = 1;
+
+  for (int i = 1; i <= x; i++) {
+    for (int c : cv) {
+      if (c <= i) dp[i] = (dp[i] + dp[i - c]) % mod;
     }
-    cout << dp[x];
+  }
+
+  cout << dp[x];
 }
--- CSES/1636 - Coin Combinations II.cpp
+++ CSES/1636 - Coin Combinations II.cpp
@@ -1,44 +1,23 @@
 #include <bits/stdc++.h>
-#define speed ios::sync_with_stdio(0);cin.tie(0)
 using namespace std;
 const int mod = 1e9 + 7;
 
 int main() {
-    speed;
-    int n, x;
-    cin >> n >> x;
-    vector<int> dp(x + 1);
-    vector<int> cv(n + 1);
-    for (int j = 1; j <= n; j++) cin >> cv[j];
-    dp[0] = 1;
-    for (int j = 1; j <= n; j++) {
-        for (int i = 0; i <= x; i++) {
-            if (i >= cv[j]) dp[i] = (dp[i] + dp[i - cv[j]]) % mod;
-        }
-    }
-    cout << dp[x];
-}
+  ios::sync_with_stdio(0), cin.tie(0);
+  int n, x;
+  cin >> n >> x;
 
-/*
-#include <bits/stdc++.h>
-#define speed ios::sync_with_stdio(0);cin.tie(0)
-using namespace std;
-const int mod = 1e9 + 7;
+  vector<int> cv(n);
+  for (auto& c : cv) cin >> c;
 
-int main() {
-    speed;
-    int n, x;
-    cin >> n >> x;
-    vector<vector<int>> dp(x + 1, vector<int>(n + 1));
-    vector<int> cv(n + 1);
-    for (int j = 1; j <= n; j++) cin >> cv[j];
-    dp[0][0] = 1;
-    for (int j = 1; j <= n; j++) {
-        for (int i = 0; i <= x; i++) {
-            dp[i][j] = dp[i][j - 1];
-            if (i >= cv[j]) dp[i][j] = (dp[i][j] + dp[i - cv[j]][j]) % mod;
-        }
+  vector<int> dp(x + 1);
+  dp[0] = 1;
+
+  for (int c : cv) {
+    for (int i = 1; i <= x; i++) {
+      if (c <= i) dp[i] = (dp[i] + dp[i - c]) % mod;
     }
-    cout << dp[x][n];
+  }
+
+  cout << dp[x];
 }
-*/
