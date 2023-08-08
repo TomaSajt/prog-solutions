cde31386a2da6cb84bc2279f98779bbb66b0211e
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-08-08 21:52:08 +0200

    Actually solve Knapsack 2

--- atcoder/dp/E - Knapsack 2.cpp
+++ atcoder/dp/E - Knapsack 2.cpp
@@ -1,19 +1,31 @@
 #include <bits/stdc++.h>
-
-long long DP[100001];
+typedef long long ll;
 using namespace std;
+const int v_max = 1000;
+
 int main() {
-    int N, W;
-    cin >> N >> W;
-    vector<long long> w(N + 1), v(N + 1);
-    for (int i = 1; i <= N; i++) cin >> w[i] >> v[i];
-    for (int j = 1; j <= N; j++) {
-        for (int i = W; i >= 0; i--) {
-            if (i >= w[j]) {
-                DP[i] = max(DP[i], DP[i - w[j]] + v[j]);
-            }
-        }
+  ll n, W;
+  cin >> n >> W;
+
+  vector<ll> v(n + 1), w(n + 1);
+  for (int i = 1; i <= n; i++) {
+    cin >> w[i] >> v[i];
+  }
+
+  vector<vector<ll>> dp(n + 1, vector<ll>(n * v_max + 1, LONG_LONG_MAX / 2));
+  dp[0][0] = 0;
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j <= n * v_max; j++) {
+      dp[i][j] = dp[i - 1][j];
+      if (j < v[i]) continue;
+      dp[i][j] = min(dp[i][j], dp[i - 1][j - v[i]] + w[i]);
     }
-    cout << DP[W] << endl;
-    return 0;
+  }
+
+  for (int j = n * v_max;; j--) {
+    if (dp[n][j] > W) continue;
+    cout << j << endl;
+    break;
+  }
 }
