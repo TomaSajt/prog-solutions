d24ef2db3981a844c6afecc6024b472fe31cc133
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-08-09 16:45:29 +0200

    Bump

--- atcoder/dp/K - Stones.cpp
+++ atcoder/dp/K - Stones.cpp
@@ -1,30 +1,23 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
   int n, k;
   cin >> n >> k;
+
   vector<int> steps(n);
   for (int& s : steps) cin >> s;
 
-  vector<bool> win(k + 1, false);
+  vector<bool> dp(k + 1, 0);
 
-  
-  for (int i = 1; i <= k; i++) {
+  for (int i = 0; i <= k; i++) {
     for (int s : steps) {
-      if (i - s >= 0 && !win[i - s]) {
-        win[i] = true;
+      if (i - s >= 0 && !dp[i - s]) {
+        dp[i] = 1;
         break;
       }
     }
   }
-
-  /*for (int i = 0; i < k; i++) {
-    if (win[i]) continue;
-    for (int s : steps) {
-      if (i + s <= k) win[i + s] = true;
-    }
-  }*/
-
-  cout << (win[k] ? "First" : "Second");
+  cout << (dp[k] ? "First" : "Second");
 }
