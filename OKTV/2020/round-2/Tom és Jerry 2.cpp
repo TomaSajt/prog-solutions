6d9b38309ca4c97b32c66e3a36cbb602d35c2170
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-09-05 09:25:37 +0200

    Bump

--- OKTV/2020/round-2/Tom és Jerry 2.cpp
+++ OKTV/2020/round-2/Tom és Jerry 2.cpp
@@ -1,11 +1,8 @@
-// refactored from mraron's code
-
 #include <bits/stdc++.h>
 using namespace std;
-const int BIG = 1e9;
 
 vector<vector<array<int, 3>>> g;
-vector<int> l, d, comp_id, tom_dist, hole_dist;
+vector<int> l, d, comp_id, tom_dist;
 int hole, ts = 0, curr_comp_id = 1;
 stack<int> st;
 
@@ -39,42 +36,11 @@ void make_comps(int u, int par_ei) {
   }
 }
 
-vector<bool> vis;
-
-// last_safe: for how many timesteps can tom move until we have to leave the current space
-vector<int> last_safe, best_tom_start;
-
-void calc_safe(int u, int par_ei_comp) {
-  vis[u] = 1;
-
-  for (auto& [v, w, ei] : g[u]) {
-    if (vis[v]) continue;
-    if (hole_dist[v] != hole_dist[u] + 1) continue;  // case: not going the shortest way
-    if (comp_id[ei] == par_ei_comp) {
-      // case: inside component
-      last_safe[v] = last_safe[u] - 1;
-      best_tom_start[v] = best_tom_start[u];
-    }
-    else {
-      // case: changing components, u is cutting vertex
-      if (tom_dist[u] <= last_safe[u]) {
-        last_safe[v] = tom_dist[u] - 1;
-        best_tom_start[v] = u;
-      }
-      else {
-        last_safe[v] = last_safe[u] - 1;
-        best_tom_start[v] = best_tom_start[u];
-      }
-    }
-    calc_safe(v, comp_id[ei]);
-  }
-}
-
 int main() {
   cin.tie(0), ios::sync_with_stdio(0);
 
-  int n, m, tom, j_cnt;
-  cin >> n >> m >> tom >> j_cnt >> hole;
+  int n, m, tom;
+  cin >> n >> m >> tom >> hole >> hole;
 
   g.resize(n + 1);
   for (int ei = 0; ei < m; ei++) {
@@ -94,39 +60,53 @@ int main() {
 
   queue<int> q;
   q.push(tom);
-  tom_dist.assign(n + 1, BIG);
+  tom_dist.assign(n + 1, INT_MAX);
   tom_dist[tom] = 0;
   while (!q.empty()) {
     int u = q.front();
     q.pop();
     for (auto& [v, w, ei] : g[u]) {
-      if (w != 2 || tom_dist[v] != BIG || v == hole) continue;
+      if (w != 2 || tom_dist[v] != INT_MAX || v == hole) continue;
       tom_dist[v] = tom_dist[u] + 1;
       q.push(v);
     }
   }
 
-  q.push(hole);
-  hole_dist.assign(n + 1, BIG);
-  hole_dist[hole] = 0;
-  while (!q.empty()) {
-    int u = q.front();
-    q.pop();
+  // last_safe: for how many timesteps can tom move until we have to leave the current space
+  vector<int> last_safe(n + 1, INT_MAX), best_tom_start(n + 1, -1);
+  vector<bool> vis(n + 1);
+  queue<pair<int, int>> q2;
+  q2.push({hole, -1});
+  vis[hole] = 1;
+  while (!q2.empty()) {
+    auto [u, par_ei_comp] = q2.front();
+    q2.pop();
     for (auto& [v, w, ei] : g[u]) {
-      if (hole_dist[v] != BIG) continue;
-      hole_dist[v] = hole_dist[u] + 1;
-      q.push(v);
+      if (vis[v]) continue;
+      vis[v] = 1;
+
+      if (comp_id[ei] == par_ei_comp) {
+        // case: inside component
+        last_safe[v] = last_safe[u] - 1;
+        best_tom_start[v] = best_tom_start[u];
+      }
+      else {
+        // case: changing components, u is cutting vertex
+        if (tom_dist[u] <= last_safe[u]) {
+          last_safe[v] = tom_dist[u] - 1;
+          best_tom_start[v] = u;
+        }
+        else {
+          last_safe[v] = last_safe[u] - 1;
+          best_tom_start[v] = best_tom_start[u];
+        }
+      }
+
+      q2.push({v, comp_id[ei]});
     }
   }
 
-  vis.resize(n + 1, 0);
-  last_safe.resize(n + 1, BIG);
-  best_tom_start.resize(n + 1, -1);
-  calc_safe(hole, -1);
-
-  while (j_cnt--) {
-    int u;
-    cin >> u;
-    cout << (last_safe[u] > 0 ? 0 : best_tom_start[u]) << '\n';
+  for (int start; cin >> start;) {
+    cout << (last_safe[start] > 0 ? 0 : best_tom_start[start]) << '\n';
   }
 }
