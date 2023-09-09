16d9d792dc2fd43d85351d37f3f44512a3543afc
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-09-10 01:43:21 +0200

    Bump

--- NT/cat-2-3/2023/round-3/Szigetek.cpp
+++ NT/cat-2-3/2023/round-3/Szigetek.cpp
@@ -3,17 +3,20 @@ using namespace std;
 
 int main() {
   cin.tie(0), cin.sync_with_stdio(0);
+
   int n;
   cin >> n;
+
   vector<int> costs(n);
-  for (auto &a : costs)
-    cin >> a;
+  for (int& a : costs) cin >> a;
+
   vector<int> dp(n);
   dp[0] = costs[0];
   dp[1] = dp[0] + costs[1];
+
   for (int i = 2; i < n; i++) {
     dp[i] = min(dp[i - 2], dp[i - 1]) + costs[i];
   }
-  cout << dp[n - 1];
-  return 0;
+
+  cout << dp.back();
 }
