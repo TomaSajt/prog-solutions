1f0dadc1c352d2ee55911c1c8b44216a263a4e2d
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-10-07 09:45:10 +0200

    Bump

--- CSES/1694 - Download Speed.cpp
+++ CSES/1694 - Download Speed.cpp
@@ -1,61 +1,57 @@
 #include <bits/stdc++.h>
-#define speed ios::sync_with_stdio(0);cin.tie(0)
 using namespace std;
 typedef long long ll;
 
 vector<vector<ll>> capacity;
-vector<vector<int>> graph;
 vector<int> parent;
 
-int n, m;
+int n;
 
-ll bfs(int s, int t) {
-    parent.assign(n + 1, -1);
-    queue<pair<int, ll>> q;
-    q.push({ s, LLONG_MAX });
-    parent[s] = -2;
-    while (!q.empty()) {
-        auto [u, flow] = q.front(); q.pop();
-        for (int v : graph[u]) {
-            if (parent[v] == -1 && capacity[u][v] > 0) {
-                parent[v] = u;
-                ll new_flow = min(flow, capacity[u][v]);
-                if (v == t) return new_flow;
-                q.push({ v, new_flow });
-            }
-        }
+ll augment(int s, int t) {
+  parent.assign(n + 1, 0);
+  queue<pair<int, ll>> q;
+  q.push({s, LLONG_MAX});
+  parent[s] = s;
+  while (!q.empty()) {
+    auto [u, flow] = q.front();
+    q.pop();
+    for (int v = 1; v <= n; v++) {
+      if (!capacity[u][v] || parent[v]) continue;
+      parent[v] = u;
+      ll new_flow = min(flow, capacity[u][v]);
+      if (v == t) return new_flow;
+      q.push({v, new_flow});
     }
-    return 0;
+  }
+  return 0;
 }
 
 ll max_flow(int s, int t) {
-    ll maxFlow = 0;
-    while (true) {
-        ll flow = bfs(s, t);
-        if (flow == 0) break;
-        int u = t;
-        maxFlow += flow;
-        while (u != s) {
-            int v = parent[u];
-            capacity[v][u] -= flow;
-            capacity[u][v] += flow;
-            u = v;
-        }
+  ll res = 0;
+  while (true) {
+    ll flow = augment(s, t);
+    if (flow == 0) break;
+    int u = t;
+    res += flow;
+    while (u != s) {
+      int v = parent[u];
+      capacity[v][u] -= flow;
+      capacity[u][v] += flow;
+      u = v;
     }
-    return maxFlow;
+  }
+  return res;
 }
 
 int main() {
-    speed;
-    cin >> n >> m;
-    capacity.resize(n + 1, vector<ll>(n + 1, 0));
-    graph.resize(n + 1);
-    while (m--) {
-        int u, v, c;
-        cin >> u >> v >> c;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-        capacity[u][v] += c;
-    }
-    cout << max_flow(1, n);
+  ios::sync_with_stdio(0), cin.tie(0);
+  int m;
+  cin >> n >> m;
+  capacity.resize(n + 1, vector<ll>(n + 1, 0));
+  while (m--) {
+    int u, v, c;
+    cin >> u >> v >> c;
+    capacity[u][v] += c;
+  }
+  cout << max_flow(1, n);
 }
