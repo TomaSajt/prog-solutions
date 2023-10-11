080e74fdfd6b22df84bbaadc37bed7ff87a6b2f9
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-10-11 21:06:41 +0200

    Refactor Tanya and Password

--- codeforces/problemset/508D - Tanya and Password.cpp
+++ codeforces/problemset/508D - Tanya and Password.cpp
@@ -1,54 +1,61 @@
 #include <bits/stdc++.h>
-#define speed ios::sync_with_stdio(0);cin.tie(0)
 using namespace std;
 
-
-map<string, multiset<string>> g;
+map<string, queue<string>> g;
 map<string, int> degdiff;
-vector<string> path;
+stack<string> path;
 
 void dfs(string u) {
-    while (!g[u].empty()) {
-        auto it = g[u].begin();
-        string v = *it;
-        g[u].erase(it);
-        dfs(v);
-    }
-    path.push_back(u);
+  while (!g[u].empty()) {
+    string v = g[u].front();
+    g[u].pop();
+    dfs(v);
+  }
+  path.push(u);
 }
 
 int main() {
-    speed;
-    int n; cin >> n;
-    for (int i = 0; i < n; i++) {
-        string s; cin >> s;
-        string u = s.substr(0, 2);
-        string v = s.substr(1, 2);
-        g[u].insert(v);
-        degdiff[u]++;
-        degdiff[v]--;
+  ios::sync_with_stdio(0), cin.tie(0);
+
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    string s;
+    cin >> s;
+    string u = s.substr(0, 2), v = s.substr(1, 2);
+    g[u].push(v);
+    degdiff[u]++, degdiff[v]--;
+  }
+
+  string start = "";
+  for (auto [k, d] : degdiff) {
+    if (abs(d) > 1) {
+      cout << "NO";
+      return 0;
     }
-    string start = "";
-    for (auto [k, d] : degdiff) {
-        if (abs(d) > 1) {
-            cout << "NO";
-            return 0;
-        }
-        if (d == 1) {
-            if (start != "") {
-                cout << "NO";
-                return 0;
-            }
-            start = k;
-        }
-    }
-    if (start == "") start = degdiff.begin()->first;
-    dfs(start);
-    if (path.size() != n + 1) {
+    if (d == 1) {
+      if (start != "") {
         cout << "NO";
         return 0;
+      }
+      start = k;
     }
-    reverse(path.begin(), path.end());
-    cout << "YES\n" << path[0][0];
-    for (auto& u : path) cout << u[1];
+  }
+  // if indeg==outdeg everywhere, choose any start vertex
+  if (start == "") start = degdiff.begin()->first;
+
+  dfs(start);
+
+  if (path.size() != n + 1) {
+    cout << "NO";
+    return 0;
+  }
+
+  cout << "YES\n";
+  cout << path.top()[0];
+  while (!path.empty()) {
+    cout << path.top()[1];
+    path.pop();
+  }
 }
