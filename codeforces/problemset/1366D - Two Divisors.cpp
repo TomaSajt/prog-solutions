3af054313869f22d0a1c7fb24735d02ce96f6849
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-10-12 20:51:23 +0200

    Bump

--- codeforces/problemset/1366D - Two Divisors.cpp
+++ codeforces/problemset/1366D - Two Divisors.cpp
@@ -1,32 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 const int MAX_A = 1e7;
 
-vector<int> primes;
-bool is_comp[MAX_A + 1];
+int prime_div[MAX_A + 1];
 
 array<int, 2> solve(int a) {
-  if (!is_comp[a]) return {-1, -1};
-  int d = 1;
-  for (int p : primes) {
-    if (a % p != 0) continue;
-    while (a % p == 0) {
-      d *= p;
-      a /= p;
-    }
-    break;
-  }
-  if (a == 1) return {-1, -1};
-  return {d, a};
+  if (prime_div[a] == a) return {-1, -1};
+  int b = a;
+  while (b % prime_div[a] == 0) b /= prime_div[a];
+  if (b == 1) return {-1, -1};
+  // these two are co-prime with eachother, so they are co-prime with their sums too
+  return {prime_div[a], b};
 }
 
 int main() {
-  ios::sync_with_stdio(0), cin.tie(0);
-  for (int i = 2; i * i <= MAX_A; i++) {
-    if (is_comp[i]) continue;
-    primes.push_back(i);
-    for (int j = i * i; j <= MAX_A; j += i) is_comp[j] = 1;
+  for (int i = 2; i <= MAX_A; i++) {
+    if (prime_div[i] != 0) continue;
+    prime_div[i] = i;
+    for (ll j = (ll)i * i; j <= MAX_A; j += i) prime_div[j] = i;
   }
+
+  ios::sync_with_stdio(0), cin.tie(0);
+
   int n;
   cin >> n;
   vector<array<int, 2>> sols(n);
