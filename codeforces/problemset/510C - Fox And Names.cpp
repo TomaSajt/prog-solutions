21edd748480b5da64036489ed9ba6795c883a3ba
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2026-03-05 11:34:14 +0100

    bump

--- codeforces/problemset/510C - Fox And Names.cpp
+++ codeforces/problemset/510C - Fox And Names.cpp
@@ -1,70 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<bool> visited(26);
-vector<unordered_set<int>> graph(26);
+vector<bool> started(26), done(26);
+vector<set<int>> graph(26);
 stack<int> s;
-vector<int> tsort;
-void dfs(int u) {
-    visited[u] = true;
-    for (auto it : graph[u]) {
-        if (!visited[it])
-            dfs(it);
-    }
-    s.push(u);
-}
-bool check_cycle() {
-    unordered_map<int, int> pos;
-    int ind = 0;
-    while (!s.empty()) {
-        pos[s.top()] = ind;
-        tsort.push_back(s.top());
-        ind += 1;
-        s.pop();
-    }
+int back_edge_cnt = 0;
 
-    for (int i = 0; i < 26; i++) {
-        for (auto it : graph[i]) {
-            if (pos[i] > pos[it]) {
-                return true;
-            }
-        }
+void dfs(int u) {
+  started[u] = true;
+  for (auto v : graph[u]) {
+    if (!started[v]) dfs(v);
+    else if (!done[v]) {
+      back_edge_cnt++;
     }
-    return false;
+  }
+  s.push(u);
+  done[u] = true;
 }
 
 int main() {
-    int n;
-    cin >> n;
-    string prev, curr;
-    cin >> prev;
-    for (int j = 1; j < n; j++) {
-        cin >> curr;
-        int minSize = min(curr.size(), prev.size());
-        for (int i = 0; i < minSize; i++) {
-            if (prev[i] != curr[i]) {
-                graph[prev[i] - 'a'].insert(curr[i] - 'a');
-                break;
-            }
-            if (i == minSize - 1 && curr.size() < prev.size()) {
-                cout << "Impossible" << endl;
-                return 0;
-            }
-        }
-        prev = curr;
-    }
+  int n;
+  cin >> n;
 
-    for (int i = 0; i < 26; i++) {
-        if (visited[i] == 0) {
-            dfs(i);
-        }
-    }
-    if (check_cycle())
-        cout << "Impossible";
-    else
-        for (auto& i : tsort)
-            cout << (char)(i + 'a');
+  string prevName, currName;
+  cin >> prevName;
+  for (int j = 1; j < n; j++) {
+    cin >> currName;
+    int minSize = min(currName.size(), prevName.size());
+    for (int i = 0; i < minSize; i++) {
+      if (prevName[i] != currName[i]) {
+        graph[prevName[i] - 'a'].insert(currName[i] - 'a');
+        break;
+      }
 
-    cout << endl;
+      if (i == minSize - 1 && currName.size() < prevName.size()) { // currName is a proper prefix of prevName
+        cout << "Impossible\n";
+        return 0;
+      }
+    }
+    prevName = currName;
+  }
+
+  for (int i = 0; i < 26; i++) {
+    if (!started[i]) dfs(i);
+  }
+
+  if (back_edge_cnt != 0) {
+    cout << "Impossible\n";
     return 0;
+  }
+
+  while (!s.empty()) {
+    cout << (char)(s.top() + 'a');
+    s.pop();
+  }
+  cout << '\n';
+  return 0;
 }
