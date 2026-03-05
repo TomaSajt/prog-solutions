6cd373d52c1455d8377344b9d484d0daff8d3904
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2026-03-05 11:17:15 +0100

    bump

--- codeforces/problemset/1217D - Coloring Edges.cpp
+++ codeforces/problemset/1217D - Coloring Edges.cpp
@@ -1,39 +1,40 @@
 #include <bits/stdc++.h>
-#define pb push_back
-#define speed ios::sync_with_stdio(0),cin.tie(0),cout.tie(0)
 using namespace std;
 
-#define int long long
-
-int n, m, a, b, kellkettooo = 0;
-vector<int> entered, leaved, is_back_edge;
-vector<vector<pair<int, int> > > g;
+vector<bool> started, done, is_back_edge;
+vector<vector<array<int, 2>>> g;
 
 void dfs(int u) {
-    entered[u] = 1;
-    for (auto w : g[u])
-        if (!entered[w.first]) dfs(w.first);
-        else if (!leaved[w.first]) {
-            is_back_edge[w.second] = 1;
-            kellkettooo = 1;
-        }
-    leaved[u] = 1;
+  started[u] = true;
+  for (auto [v, i] : g[u]) {
+    if (!started[v]) dfs(v);
+    else if (!done[v]) {
+      is_back_edge[i] = true;
+    }
+  }
+  done[u] = true;
 }
 
-#undef int
 int main() {
-#define int long long
-    speed;
-    cin >> n >> m;
-    g.resize(n + 1), entered.resize(n + 1), leaved.resize(n + 1), is_back_edge.resize(m);
-    for (int i = 0; i < m; i++) {
-        cin >> a >> b;
-        g[a].pb({ b, i });
-    }
-    for (int i = 1; i <= n; i++) if (!entered[i]) dfs(i);
-    cout << kellkettooo + 1 << endl;
-    for (int i = 0; i < m; i++) {
-        cout << is_back_edge[i] + 1 << " ";
-    }
-    cout << endl;
+  int n, m;
+  cin >> n >> m;
+  g.resize(n + 1), started.resize(n + 1), done.resize(n + 1), is_back_edge.resize(m);
+
+  for (int i = 0; i < m; i++) {
+    int u, v;
+    cin >> u >> v;
+    g[u].push_back({v, i});
+  }
+
+  for (int i = 1; i <= n; i++) {
+    if (!started[i]) dfs(i);
+  }
+
+  cout << (count(is_back_edge.begin(), is_back_edge.end(), true) > 0 ? 2 : 1)
+       << endl;
+
+  for (int i = 0; i < m; i++) {
+    cout << (is_back_edge[i] ? 2 : 1) << " ";
+  }
+  cout << endl;
 }
